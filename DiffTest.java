367613e5fdf60c3a09b752fb8e7d82ddcfbc4f88
vivien <vivien.x.liang@gmail.com>
2013-03-04 15:19:04 +0100

    java basic functional code for DiffTest
    reference: warm-up diff21

---
+++ DiffTest.java
@@ -0,0 +1,13 @@
+class DiffTest{
+
+public int diff21(int n) {
+
+return 0;
+}
+
+public static void main (String[] args) {
+	DiffTest d = new DiffTest();
+	System.out.println("The result of diff21(" + args[0] + ") is: " + d.diff21(Integer.parseInt(args[0])));
+}
+
+}
